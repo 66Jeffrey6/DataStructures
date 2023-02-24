cf111615557b508c3ff5014e9d8137ff38aff884
66Jeffrey6 <125640761+66Jeffrey6@users.noreply.github.com>
2023-02-23 23:36:48 -1200

    Add files via upload

---
+++ Jeffrey Kwame Donkor 6935321.py
@@ -0,0 +1,52 @@
+#Currency used is Great British Pound
+Toyota = ("Camry","Corolla","Highlander")
+Lamborghini ={"Aventador","Gallardo","SestoElemento"}
+Honda ={"Civic ","CR-V","Accord"}
+Ford ={"Explorer","Escape","Everest"}
+Bugatti ={"Veyron","Chiron","Divo"}
+Hyundai ={"Elantra","Sonata","Accent"}
+Nissan ={"Rogue","Pathfinder","Patrol"}
+Benz ={"Gia","G-Wagon","S600"}
+Chevrolet ={"Corvette","Camaro","Malibu"}
+BMW ={"BMW X2","BMW X4","BMWiX"}
+carType =  (input("Please enter the car type:"))
+if carType is Lamborghini:
+    print ("The price of model Aventador is 10.00")
+    print ("The price of model Gallardo is 10.00")
+    print ("The price of model Sesto Elemento is 10.00" )
+elif carType is Toyota:
+    print("The price of Corolla is 5.00")
+    print("The price of Camry is 5.00")
+    print("The price of Highlander is 5.00")
+elif carType is Honda:
+    print("The price of Civic is 5.00")
+    print("The price of CR-V  is 5.00")
+    print("The price of Accord is 5.00")
+elif carType is Ford:
+    print("The price of Explorer is 5.00")
+    print("The price of Escape is 5.00")
+    print("The price of Everest is 5.00")    
+elif carType is Bugatti:
+     print("The price of Veyron is 30.00")
+     print("The price of Chiron is 25.00")
+     print("The price of Divo is 25.00")   
+elif carType is Hyundai:
+    print("The price of Elantra is 15.00")
+    print("The price of Sonata is 11.00")
+    print("The price of Accent is 10.00")  
+elif carType is Nissan:
+    print("The price of Rogue is 10.00")
+    print("The price of Pathfinder is 10.00")
+    print("The price of Patrol is 5.00")
+elif carType is Benz:
+    print("The price of Gia is 30.00")
+    print("The price of G-Wagon is 30.00")
+    print("The price of S600 is 28.00")
+elif carType is  Chevrolet:
+    print("The price of Corvette is 15.00")
+    print("The price of Camaro is 15.00")
+    print("The price of Malibu is 5.00")
+elif carType is BMW:
+    print("The price of BMW X2 is 10.00")
+    print("The price of BMW X4 is 10.00")
+    print("The price of BMW iX is 2.00")
